f412e6f70ee92086e97ac7fbc1fbb86dc7e327e7
HillarKapsta <hillar@digitaltech.ee>
2022-09-10 19:15:18 +0300

    First content

---
+++ src/main/java/com/kapsta/sectorpickerback/SectorpickerBackApplication.java
@@ -0,0 +1,13 @@
+package com.kapsta.sectorpickerback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SectorpickerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SectorpickerBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kapsta/sectorpickerback/SectorpickerBackApplicationTests.java
@@ -0,0 +1,14 @@
+package com.kapsta.sectorpickerback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SectorpickerBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assert(true);
+	}
+
+}
